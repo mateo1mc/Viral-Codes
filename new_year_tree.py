0e2db9ef74bfbab0906576bc2f1be56fb21163dd
MATEO C <112934949+mateo1mc@users.noreply.github.com>
2024-02-29 19:46:48 +0100

    Create new_year_tree.py

---
+++ new_year_tree.py
@@ -0,0 +1,17 @@
+# Print the tree part 1
+i = 1
+while i <= 5:
+    print(" " * (5 - i) + "*" * (2*i - 1))
+    i += 1
+
+# Print the trunk of the tree part 2
+print(" " * 4 + "||")
+
+
+# output will look like this:
+#     *
+#    ***
+#   *****
+#  *******
+# *********
+#     ||
