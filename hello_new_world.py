953778c19e63e4e2ddc363c1959670e268b40379
MATEO C <112934949+mateo1mc@users.noreply.github.com>
2024-03-25 16:31:40 +0100

    Update hello_new_world.py

--- hello_new_world.py
+++ hello_new_world.py
@@ -1,1 +1,14 @@
+import string
+import time
 
+text = "Hello World!"
+temp = ''
+for ch in text:
+    for i in string.printable:
+        if i == ch or ch == i:
+            time.sleep(0.02)
+            print(temp+i)
+            temp+=ch
+        else:
+            time.sleep(0.02)
+            print(temp+i)
